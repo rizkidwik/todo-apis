dfb2968d52ed130ae22574f496d53e5588281809
rizkidwik <rizkidwi123@gmail.com>
2022-08-26 22:13:32 +0700

    crud todo

--- controller/Todos.js
+++ controller/Todos.js
@@ -1,6 +1,6 @@
 import Todos from "../model/TodoModel.js";
-
-
+import jwt from "jsonwebtoken";
+import Users from "../model/UserModel.js";
 export const getTodos = async(req,res)=>{
     try {
         const todos = await Todos.findAll({
@@ -13,5 +13,70 @@ export const getTodos = async(req,res)=>{
 }
 
 export const addTodo = async(req,res)=>{
-    // const {todo,completed,}
+    const todo= req.body.todo;
+    const completed = req.body.completed;
+    if(!todo || !completed) return res.status(400).json({
+        msg: "Field wajib diisi"
+    })
+    // return res.json({todo,completed});
+    const refreshToken = req.cookies.refreshToken;
+    if(!refreshToken) return res.sendStatus(204);
+    const user = await Users.findAll({
+        where:{
+            refresh_token:refreshToken
+        }
+    });
+    if(!user[0]) return res.sendStatus(204);
+    const userId = user[0].id;
+    try {
+        await Todos.create({
+            todo:todo,
+            completed:completed,
+            userId:userId
+        })
+        res.json({
+            msg: "Data berhasil ditambahkan"
+        });
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(401).json({msg:"Data gagal ditambahkan"});
+    }
+}
+
+export const updateTodo = async(req,res)=>{
+    const {todo,completed} = req.body;
+    if(!todo || !completed) return res.sendStatus(400).json({msg: "Field wajib diisi"});
+    try {
+        await Todos.update({
+            todo:todo,
+            completed:completed,
+        },
+        {
+            where: {
+                id:req.params.id,
+            }
+        },
+        {
+            timestamps:true,
+            updatedAt: 'updateTimestamp'
+        });
+        return res.json({
+            msg: "Data berhasil diupdate"
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const deleteTodo = async(req,res)=>{
+    try {
+        await Todos.destroy({
+            where: {
+                id:req.params.id
+            }
+        });
+        return res.json({msg: "Data terhapus"})
+    } catch (error) {
+        console.log(error);
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {getUsers,Register,Login,Logout } from "../controller/Users.js";
-import {getTodos} from "../controller/Todos.js"
+import {getTodos,addTodo,updateTodo,deleteTodo} from "../controller/Todos.js"
 import {verifyToken } from "../middleware/VerifyToken.js";
 import {refreshToken} from "../controller/RefreshToken.js";
 
@@ -12,5 +12,7 @@ router.post('/login',Login);
 router.get('/token', refreshToken);
 router.delete('/logout',Logout);
 router.get('/todos',getTodos);
-
+router.post('/todos',addTodo);
+router.put('/todos/:id',updateTodo);
+router.delete('/todos/:id',deleteTodo);
 export default router;
